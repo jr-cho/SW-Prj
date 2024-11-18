23f21dc62628fdcafcb8a24a5a1c141031a78469
jr-cho <joshmg426@gmail.com>
2024-11-17 22:36:13 -0500

    updated main.py and added README.md

---
+++ README.md
@@ -0,0 +1,17 @@
+# Sine Wave Plot
+
+## Description
+Creates a simple plot for a Sine Wave!
+
+## Installation
+```
+pip install -r requirements.txt
+```
+
+## Usage
+```
+py main.py
+```
+
+## More
+You can find more of my work at [jr-cho](https://github.com/jr-cho)
--- main.py
+++ main.py
@@ -1,3 +1,15 @@
 import numpy as np
 import matplotlib.pyplot as plt
 
+def plot():
+    x = np.linspace(0, 2 * np.pi, 1000)
+    y = np.sin(x)
+    plt.plot(x, y)
+    plt.title('Sine Wave')
+    plt.xlabel('Angle (radians)')
+    plt.ylabel('Amplitude')
+    plt.grid()
+    plt.show()
+
+if __name__ == "__main__":
+    plot()
